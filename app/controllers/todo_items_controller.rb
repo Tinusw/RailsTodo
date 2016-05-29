65fc87722fcaa1a45eaa22a896d23b1e9c63f31b
Tinus <vertelmy@gmail.com>
2016-05-29 14:51:42 +1200

    added method to delete task in items controller and html page.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,5 @@
 class TodoItemsController < ApplicationController
-	before action :set_todo_list
+	before_action :set_todo_list
 
 	def create
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -7,11 +7,22 @@ class TodoItemsController < ApplicationController
 		redirect_to @todo_list
 	end
 
+	def destroy
+		@todo_item = @todo_list.todo_items.find(params[:id])
+		if @todo_item.destroy
+			flash[:success] = "Deleted"
+		else
+			flash[:error] = "Can't delete!"
+		end
+		redirect_to @todo_list
+	end
+
 	private 
 
 	def set_todo_list
-		@todo_list = todo_list.find(params[:todo_list_id])
+		@todo_list = TodoList.find(params[:todo_list_id])
 	end
+
 
 	def todo_item_params
 		params[:todo_item].permit(:content, :desc)
